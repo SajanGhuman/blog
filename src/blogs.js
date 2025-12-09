3c3f2d3336354e7278be1f256b1ed2e315f30e4d
Sajanpreet Singh <sajanghuman18@gmail.com>
2025-12-09 11:37:40 -0600

    Fixed global styling

--- src/blogs.js
+++ src/blogs.js
@@ -5,34 +5,4 @@ export const blog = [
     date: "2023-09-02t12:00:00-0400",
     desc: "this is a sample description to check how well the component behaves with long string",
   },
-  {
-    title: "sample",
-    slug: "sample",
-    date: "2023-09-02t12:00:00-0400",
-    desc: "this is a sample description to check how well the component behaves with long string",
-  },
-  {
-    title: "sample",
-    slug: "sample",
-    date: "2023-09-02t12:00:00-0400",
-    desc: "this is a sample description to check how well the component behaves with long string",
-  },
-  {
-    title: "sample",
-    slug: "sample",
-    date: "2023-09-02t12:00:00-0400",
-    desc: "this is a sample description to check how well the component behaves with long string",
-  },
-  {
-    title: "sample",
-    slug: "sample",
-    date: "2023-09-02t12:00:00-0400",
-    desc: "this is a sample description to check how well the component behaves with long string",
-  },
-  {
-    title: "sample",
-    slug: "sample",
-    date: "2023-09-02t12:00:00-0400",
-    desc: "this is a sample description to check how well the component behaves with long string",
-  },
 ];
